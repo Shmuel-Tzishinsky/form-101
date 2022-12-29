9f97d05be3074bbabedb6dec3fd40d346c506051
Shmuel Tzishinsky <a0527188257@gmail.com>
2022-12-29 02:05:41 +0200

    remove client files

--- client/src/components/loading/Loading-login.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './loading-login.css';
-
-function LoadingLogin() {
-  return <div className="loading-login loading-login-full-height"></div>;
-}
-
-export default LoadingLogin;
